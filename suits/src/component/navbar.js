d3c0ea6aa62a01931216451acc099a3f69c06ef7
Mopeko <maxime.brault@epitech.digital>
2024-01-19 14:20:50 +0100

    [Add] Strapi

--- suits/src/component/navbar.js
+++ suits/src/component/navbar.js
@@ -8,12 +8,12 @@ const { Footer } = Layout;
 const NavBar = () => {
   return (
     <Footer style={{ position: 'fixed', bottom: 0, width: '100%', padding: 0 }}>
-      <div style={{ display: 'flex', justifyContent: 'space-evenly', borderRadius: '15px' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-evenly'}}>
         <Menu
           theme="dark"
           mode="horizontal"
           defaultSelectedKeys={['home']}
-          style={{ borderRadius: '15px', width: '100%', textAlign: 'center', lineHeight: '64px' }}
+          style={{ width: '100%', textAlign: 'center', lineHeight: '64px' }}
         >
           <Menu.Item key="home" icon={<HomeOutlined />} style={{ borderRadius: '15px', outline:'none', ':focus': { outline: 'none' } }} />
           <Menu.Item key="search" icon={<SearchOutlined />} style={{ borderRadius: '15px', outline:'none' }} />
